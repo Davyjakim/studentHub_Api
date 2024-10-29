0c54f3fe53edb2d61ff6db1860ee405de95fa99d
Davyjakim <jakimdavy07@gmail.com>
2024-10-29 10:36:58 +0100

    cors issue solved

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 require(`dotenv`).config({ path: `./config/.env` });
 const config = require("config");
 
-const client_url= 'http://localhost:3000'
+const client_url= 'https://student-hub-lime.vercel.app'
 
 const corsOptions = {
   origin: client_url, 
